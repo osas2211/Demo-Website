4e8676379b779f726026d7722c5d16ff27b22d2f
JoE11-y <Josephedoh77@gmail.com>
2024-02-28 13:30:04 +0100

    tailwind fix and skeletons

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
 
 export const LandingPage = () => {
-    return <div>LandingPage</div>
+    return <div className="bg-[#000] min-h-[100vh]"></div>
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,19 @@
-import Head from "next/head";
-import GradientBG from "./GradientBG.js";
-import styles from "../styles/Home.module.css";
+import Head from "next/head"
+import GradientBG from "./GradientBG.js"
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Head>
         <title>MVS-service</title>
-        <meta name="description" content="Mina Verification Service, a project that provides advanced identity verification and authentication." />
+        <meta
+          name="description"
+          content="Mina Verification Service, a project that provides advanced identity verification and authentication."
+        />
         <link rel="icon" href="/assets/favicon.ico" />
       </Head>
-      <GradientBG>{children}</GradientBG>
+      {/* <GradientBG></GradientBG> */}
+      <div>{children}</div>
     </>
-  );
+  )
 }
