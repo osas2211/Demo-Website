209d54f5354f5205bde8fab4e7a24f5117bfaf18
Osaretin Frank <osaretin.frank10@gmail.com>
2024-03-13 11:21:00 +0100

    finish landing page hero desktop version

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,20 @@ import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className="relative monument bg-custom-black text-custom-white min-h-screen">
-      <div className="px-[1rem] md:w-[95%] mx-auto">
+    <main className="relative monument bg-custom-black text-custom-white min-h-screen overflow-x-hidden no-scrollbar">
+      <div className="px-[1rem] md:w-[92%] mx-auto">
         <Header />
         <LandingPageHero />
       </div>
-      <div className="absolute top-[50%] left-[40%] translate-x-[50%] translate-y-[-50%]">
+      <div className="absolute top-[50%] left-[40%] translate-x-[50%] translate-y-[-40%]">
         <div
           className="h-[600px] w-[100px] bg-primary -rotate-[45deg]"
+          style={{ filter: "blur(100px)" }}
+        ></div>
+      </div>
+      <div className="absolute bottom-[-250px] right-[-150px]">
+        <div
+          className="h-[300px] w-[300px] bg-primary"
           style={{ filter: "blur(100px)" }}
         ></div>
       </div>
--- components/LandingPageHero.tsx
+++ components/LandingPageHero.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import { Button } from "./Button"
+import { FiArrowUpRight } from "react-icons/fi"
+import { PiSealCheckFill } from "react-icons/pi"
 
 export const LandingPageHero = () => {
   return (
-    <div className="relative md:mt-[2.5rem] mt-[1.5rem] z-[10]">
+    <div className="relative md:mt-[0.5rem] mt-[1rem] z-[10]">
       <div className="flex items-end gap-4 justify-between">
-        <h2 className="text-[5rem] leading-[6rem] uppercase font-[400]">
-          Mina verification service
+        <h2 className="text-[4rem] leading-[5rem] uppercase font-[500]">
+          <span className="special-text">Mina</span> verification service
         </h2>
         <div className="md:w-[80%]">
           <p className="text-[18px] mb-5">
@@ -15,6 +17,73 @@ export const LandingPageHero = () => {
             tools and continously maintained.
           </p>
           <Button variant="secondary">MVS Tech Stack</Button>
+        </div>
+      </div>
+      <div className="mt-[3.5rem] text-custom-black">
+        <div className="grid md:grid-cols-3 md:min-h-[54vh] gap-2">
+          <div className="rounded-lg col-span-1 bg-custom-white h-full w-full px-5 py-6">
+            <div className="flex flex-col justify-end items-end mb-[3rem] gap-4">
+              <p className="bg-custom-black/90 rounded-lg px-4 py-3 text-custom-white inline-block">
+                <span>Social Good</span>
+                <PiSealCheckFill className="text-primary ml-1 inline" />
+              </p>
+              <p className="bg-custom-black/90 rounded-lg px-4 py-3 text-custom-white inline-block">
+                Built for Social{" "}
+                <span className="underline text-primary">Security</span>
+              </p>
+            </div>
+            <div>
+              <h3 className="text-4xl font-[500]">Introducing MVS</h3>
+              <div className="px-4 py-5">
+                <p className="p-4 border-l-[4px] border-l-custom-black">
+                  Mina Verification Service, a project that provides advanced
+                  identity verification and authentication measures to safeguard
+                  the security of web3 platforms using Mina&apos;s
+                  zero-knowledge proofs down approach, to provide a secure and
+                  scalable solution for private applications in the web3 space.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="rounded-lg col-span-2 h-full w-full grid md:grid-col-2 gap-2">
+            <div className="rounded-lg col-span-2 bg-custom-white md:min-h-[20vh] w-full px-5 py-1">
+              <div className="flex justify-between items-center text-custom-black/90">
+                <h2 className="text-4xl font-[500]">
+                  Building the Unstoppable
+                </h2>
+                <FiArrowUpRight className="text-[5.6rem]" />
+              </div>
+              <p className="text-lg md:w-[50%]">
+                A scalable network powering a global web3 vision for all.
+              </p>
+            </div>
+            <div className="rounded-lg bg-custom-white md:min-h-[34vh] w-full px-5 py-6">
+              <div className="flex flex-col justify-between items-end h-full">
+                <div>
+                  <h2 className="text-4xl font-[500] text-custom-black/90 mb-3">
+                    2024 Starmap
+                  </h2>
+                  <p className="text-lg">
+                    Embarking on a new journey to reach our full potential.
+                  </p>
+                </div>
+                <FiArrowUpRight className="text-[5.6rem]" />
+              </div>
+            </div>
+            <div className="rounded-lg bg-primary md:min-h-[34vh] w-full px-5 py-6">
+              <div className="flex flex-col justify-between items-end h-full">
+                <div>
+                  <h2 className="text-4xl font-[500] text-custom-black/90 mb-3">
+                    MVS
+                  </h2>
+                  <p className="text-lg">
+                    Learn about how MVS was born and changing the web3 space.
+                  </p>
+                </div>
+                <FiArrowUpRight className="text-[5.6rem]" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
